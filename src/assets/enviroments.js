a16ba6772e43258f27b212676faabe838e3354a6
vasawat <vasawat77@gmail.com>
2024-09-16 16:59:18 +0700

    api

--- src/assets/enviroments.js
+++ src/assets/enviroments.js
@@ -1,5 +1,7 @@
 const enviroment = {
-    apiUrl: "http://localhost:5000",
+    // apiUrl: "http://localhost:5000",
+    apiUrl: "https://thatako-ptyk-backend.onrender.com",
+
 };
 
 export default enviroment;
