98b43c112717007714e97fa850e6e824c1009c8e
vasawat <vasawat77@gmail.com>
2024-09-16 16:54:09 +0700

    depedency2

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.css";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate  } from "react-router-dom";
 
