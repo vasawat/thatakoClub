de2d0a5b06c48ee4b9788692b7d342222798b9a7
vasawat prapachaimongkol <vasawat.ppcmk@gmail.com>
2024-10-21 12:20:05 +0700

    railway

--- src/assets/enviroments.js
+++ src/assets/enviroments.js
@@ -1,7 +1,7 @@
 const enviroment = {
     // apiUrl: "http://localhost:5000",
-    apiUrl: "https://thatako-ptyk-backend.onrender.com",
-    // apiUrl: "https://thatakoptykbackend-production.up.railway.app"
+    // apiUrl: "https://thatako-ptyk-backend.onrender.com",
+    apiUrl: "https://thatakoptykbackend-production.up.railway.app"
 
 };
 
